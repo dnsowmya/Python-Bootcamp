7dc3148f72b351af12ec86d6e9a8be4b68a8a773
Sowmya Devarapu <d.n.sowmya@gmail.com>
2023-10-24 11:15:00 -0500

    Errors and Exceptions Handling

---
+++ simple1.py
@@ -0,0 +1,16 @@
+'''
+A very simple script
+'''
+
+def myfunc():
+    '''
+    A simple function
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
+
+#pylint is a static code analyser for Python 2 or 3. The latest version supports Python 3.8.0 and above. Pylint analyses your code without actually running it.
